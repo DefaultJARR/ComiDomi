ce51850c601cbc2cefab937b8b42db5b3411ebf9
DefaultJARR <rrja98@gmail.com>
2019-10-24 17:42:43 -0500

    # Comit Changes: - Avances en la Screen de Restaurantes
    Nota:
    - Errores en el Uso del FlatList que lista los Restaurantes, mas exactamente en las funciones "handleLoadMore" de (Restaurantes.js)

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList, ActivityIndicator, TouchableOpacity, TouchableWithoutFeedbackBase } from 'react-native';
+import { Image } from 'react-native-elements';
 
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -17,7 +18,8 @@ export default class Restaurants extends Component {
             login: false,
             restaurants: null, // Array de Restaurantes
             startRestaurants: null, // Restaurantes a utlizar
-            limitRestaurants: 8 // limite de cantidad de restaurantes a traer de la base de Datos
+            limitRestaurants: 8, // limite de cantidad de restaurantes a traer de la base de Datos
+            isLoading: true // Indica la carga de Restaurantes
         }
     }
 
@@ -80,14 +82,107 @@ export default class Restaurants extends Component {
                 this.setState({ restaurants: resultRestaurants });
             })
 
-        console.log("Wooooooow: \n", this.state.restaurants);
+    }
 
+    // s
+    handleLoadMore = async () => {
+        console.log("GASDGADAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+        const { restaurants, limitRestaurants, startRestaurants } = this.state;
+        let resultrestaurants = restaurants;
+
+        const restaurantsDB = db.collection("restaurants")
+            .orderBy("createAt", "desc")
+            .startAfter(startRestaurants.data().createAt)
+            .limit(limitRestaurants);
+
+        await restaurantsDB.get()
+            .then(response => {
+                response.forEach(doc => {
+                    console.log(doc.data);
+                });
+            })
+    }
+
+    renderRow = restaurant => {
+        const { name, city, address, description, image } = restaurant.item.restaurant;
+
+        return (
+            <TouchableOpacity onPress={() => this.clickRestaurant(restaurant)}>
+                <View style={styles.viewRestaurantItem}>
+                    <View style={styles.viewImageRestaurantItem}>
+                        <Image
+                            resizeMode="cover"
+                            source={{ uri: image }}
+                            style={styles.imageRestaurantItem}
+                        />
+                    </View>
+                    <View>
+                        <Text style={styles.flatListRestaurantName}>{name}</Text>
+                        <Text style={styles.flatListRestaurantAddress}>
+                            {city}, {address}
+                        </Text>
+                        <Text style={styles.flatListRestaurantDescription}>
+                            {description.substr(0, 60)}...
+                        </Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    renderFooter = () => {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.viewLoaderRestaurants}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
+        }
+        else {
+            return (
+                <View style={styles.viewNotFoundRestaurants}>
+                    <Text>No quedan Restaurantes por Cargar</Text>
+                </View>
+            );
+        }
+    }
+
+    renderFlatList = restaurants => {
+        if (restaurants) {
+            return (
+                <FlatList
+                    data={this.state.restaurants}
+                    renderItem={this.renderRow}
+                    initialNumToRender={this.state.limitRestaurants}
+                    keyExtractor={(item, index) => index.toString()}
+                    onEndReached={this.handleLoadMore()}
+                    onEndReachedThreshold={-1}
+                    ListFooterComponent={this.renderFooter}
+                />
+            )
+        }
+        else {
+            return (
+                <View style={styles.loadingRestaurants}>
+                    <ActivityIndicator size="large" />
+                    <Text>Cargando Restaurantes</Text>
+                </View>
+            )
+        }
+    }
+
+    clickRestaurant = restaurant => {
+        console.log("Click en el Restaurante: ");
+        console.log(restaurant);
     }
 
     render() {
+        const { restaurants } = this.state;
+
         return (
             <View style={styles.viewBody}>
-                <Text>Restaurants Screen...</Text>
+                {this.renderFlatList(restaurants)}
                 {this.showActionButton()}
             </View>
         );
@@ -96,14 +191,46 @@ export default class Restaurants extends Component {
 
 const styles = StyleSheet.create({
     viewBody: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
+        flex: 1
     },
     actionButtonIcon: {
         fontSize: 20,
         height: 22,
         color: 'white',
+    },
+    loadingRestaurants: {
+        marginTop: 20,
+        alignItems: "center"
+    },
+    viewRestaurantItem: {
+        flexDirection: "row",
+        margin: 10
+    },
+    viewImageRestaurantItem: {
+        marginRight: 15
+    },
+    imageRestaurantItem: {
+        width: 80,
+        height: 80
+    },
+    flatListRestaurantName: {
+        fontWeight: "bold"
+    },
+    flatListRestaurantAddress: {
+        paddingTop: 2,
+        color: "gray"
+    },
+    flatListRestaurantDescription: {
+        paddingTop: 2,
+        color: "gray",
+        width: 250
+    },
+    viewLoaderRestaurants: {
+        marginTop: 15,
+        marginBottom: 15
+    },
+    viewNotFoundRestaurants: {
+        marginVertical: 15,
+        alignItems: "center"
     }
 });
